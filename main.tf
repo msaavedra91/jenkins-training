15dbc3952cb28ee052ef90f1fef61f60178ad40f
Miguel Saavedra <miguelernesto911@gmail.com>
2020-04-29 19:07:46 -0500

    Change bucket name

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket-micky-19287319837892137"
   acl    = "private"
 
   tags = {
